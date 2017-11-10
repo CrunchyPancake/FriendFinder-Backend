98d3251d8e64e6afb0aa9d83d902544a8eec0480
Jonathan <jonathan.henriksen@yandex.com>
2017-11-10 21:31:04 +0100

    cleaned up

--- app.js
+++ app.js
@@ -14,9 +14,6 @@ app.post('/', function (req, res) {
   let user = req.body.userName;
   let coordinates = req.body.coordinates;
   let distance = req.body.distance;
-  console.log("Username: " + user);
-  console.log("coordinates: " + coordinates);
-  console.log("Distance: " + distance);
   register(user, coordinates, distance, function(err, docs) {
     if (err) {
       return console.log(err);
